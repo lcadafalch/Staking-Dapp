b6061bad493fa35bc6d38f63952f6d8333463e9f
lcadafalch <56115195+lcadafalch@users.noreply.github.com>
2022-03-22 09:55:28 +0100

    truffle config inicial
    Config truffle estandar para proyecto solidity react ganache

---
+++ truffle-config.js
@@ -0,0 +1,27 @@
+// CONFIGURACIÓN INICIAL PARA TRUFFLE
+require('babel-register');
+require('babel-polyfill');
+
+module.exports = {
+    networks:{
+        development:{
+            host:'127.0.0.1:',
+            port:'7545',
+            network_id:'*'  // cualquier network k haya
+
+        },
+
+    },
+    contracts_directory:'./src/contracts/',
+    contracts_build_directory:'./src/truffle_abis',
+    compilers:{
+        solc:{
+            version:'^0.5.0',
+            optimizer:{
+                enabled:true,
+                runs:200,
+
+            }
+        }
+    }
+}
